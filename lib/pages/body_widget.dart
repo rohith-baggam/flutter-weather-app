d94b3d8ed4e3aa1ad64dbd75723e876f61299a08
rohith-baggam <rohith.baggam@aptagrim.com>
2025-06-28 13:41:34 +0530

    API's integrated

--- lib/pages/body_widget.dart
+++ lib/pages/body_widget.dart
@@ -1,12 +1,64 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:weather_app/widgets/aditional_information.dart';
 import 'package:weather_app/widgets/inner_card.dart';
 import 'package:weather_app/widgets/main_card_widget.dart';
 
 class BodyWidget extends StatelessWidget {
-  const BodyWidget({super.key});
+  final Future future;
+  const BodyWidget({super.key, required this.future});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: future,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator.adaptive());
+        }
+        if (snapshot.hasError) {
+          return Center(child: Text(snapshot.error.toString()));
+        }
+        final data = snapshot.data;
+        final results = data['list'];
+        final currentWeatherData = data['list'][0];
+        final currentTemperature = currentWeatherData['main']['temp'];
+        final currentSky = currentWeatherData['weather'][0]['main'];
+        final currentPressure = currentWeatherData['main']['pressure']
+            .toString();
+        final currentWindSpeed = currentWeatherData['wind']['speed'].toString();
+        final currentHumidity = currentWeatherData['main']['humidity']
+            .toString();
+
+        return BodyPaddingWidget(
+          temperature: currentTemperature,
+          currentSky: currentSky,
+          currentPressure: currentPressure,
+          currentWindSpeed: currentWindSpeed,
+          currentHumidity: currentHumidity,
+          results: results,
+        );
+      },
+    );
+  }
+}
+
+class BodyPaddingWidget extends StatelessWidget {
+  const BodyPaddingWidget({
+    super.key,
+    required this.temperature,
+    required this.currentSky,
+    required this.currentPressure,
+    required this.currentWindSpeed,
+    required this.currentHumidity,
+    required this.results,
+  });
+
+  final double temperature;
+  final String currentSky;
+  final String currentPressure;
+  final String currentWindSpeed;
+  final String currentHumidity;
+  final List<dynamic> results;
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +68,7 @@ class BodyWidget extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // main card
-          MainCardWidget(),
+          MainCardWidget(temperature: temperature, currentSky: currentSky),
 
           SizedBox(height: 16),
           Text(
@@ -26,53 +78,7 @@ class BodyWidget extends StatelessWidget {
           SizedBox(height: 20),
           // weather forcast
           // Row scrollable widget
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 100,
-                  child: HourlyForecastWidget(
-                    icon: Icons.cloud,
-                    time: "00:00",
-                    temperature: "301.22",
-                  ),
-                ),
-                SizedBox(
-                  width: 100,
-                  child: HourlyForecastWidget(
-                    icon: Icons.sunny,
-                    time: "03:00",
-                    temperature: "300.52",
-                  ),
-                ),
-                SizedBox(
-                  width: 100,
-                  child: HourlyForecastWidget(
-                    icon: Icons.cloud,
-                    time: "06:00",
-                    temperature: "302.22",
-                  ),
-                ),
-                SizedBox(
-                  width: 100,
-                  child: HourlyForecastWidget(
-                    icon: Icons.sunny,
-                    time: "09:00",
-                    temperature: "300.12",
-                  ),
-                ),
-                SizedBox(
-                  width: 100,
-                  child: HourlyForecastWidget(
-                    icon: Icons.cloud,
-                    time: "21:00",
-                    temperature: "299.8",
-                  ),
-                ),
-              ],
-            ),
-          ),
+          WeatherForecastScrollWidget(results: results),
           SizedBox(height: 20),
           Text(
             "Additional Information",
@@ -86,22 +92,57 @@ class BodyWidget extends StatelessWidget {
               AdditionInfoWidget(
                 icon: Icons.water_drop,
                 label: "Humidity",
-                value: "91",
+                value: currentHumidity,
               ),
               AdditionInfoWidget(
                 icon: Icons.air,
                 label: "Wind Speed",
-                value: "7.5",
+                value: currentWindSpeed,
               ),
               AdditionInfoWidget(
                 icon: Icons.beach_access,
                 label: "Pressure",
-                value: "1000",
+                value: currentPressure,
               ),
             ],
           ),
         ],
       ),
+    );
+  }
+}
+
+class WeatherForecastScrollWidget extends StatelessWidget {
+  const WeatherForecastScrollWidget({super.key, required this.results});
+  final List<dynamic> results;
+
+  List<SizedBox> getWeatherForecast() {
+    List<SizedBox> data = [];
+    for (int i = 1; i < 10; i++) {
+      dynamic mainData = results[i]['main'];
+      final weather = results[i]['weather'][0]['main'].toString();
+      final time = results[i]['dt_txt'].toString().substring(11, 16);
+      data.add(
+        SizedBox(
+          width: 100,
+          child: HourlyForecastWidget(
+            icon: weather == "Clouds" || weather == "Rain"
+                ? Icons.cloud
+                : Icons.sunny,
+            time: time,
+            temperature: mainData['temp'].toString(),
+          ),
+        ),
+      );
+    }
+    return data;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(children: getWeatherForecast()),
     );
   }
 }
